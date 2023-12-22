69c839e50a26862217c858f89e0dfdf00997b36a
Chhavi Prabhakar <prabhakarchhavi9@gmail.com>
2023-12-23 00:37:03 +0530

    added new components ExpenseDetails and made my code more readable and shorter.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 
 function ExpenseItem(props) {
-
   return (
-    <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <h2>at {props.location}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        title={props.title}
+        location={props.location}
+        amount={props.amount}
+      />
     </div>
   );
 }
