e271949ebb073e5fef207ec6bc641325a260bfeb
Chhavi Prabhakar <prabhakarchhavi9@gmail.com>
2023-12-22 13:17:51 +0530

    done the given tasks without hints

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense item!</h2>
+      <p>Food Rs10</p>
+      <p>Petrol Rs100</p>
+      <p>Movie Rs200</p>
+    </div>
+  );
+}
+
+export default ExpenseItem;
