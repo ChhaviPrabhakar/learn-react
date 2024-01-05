af91d61dedb6e1fad4193c524e8a29fcb1e7c137
Chhavi Prabhakar <prabhakarchhavi9@gmail.com>
2024-01-05 22:37:24 +0530

    added Chart Bar of expenses to UI dynamically

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesChart from "./ExpensesChart";
 import { useState } from "react";
 import "./Expenses.css";
 
@@ -21,6 +22,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
